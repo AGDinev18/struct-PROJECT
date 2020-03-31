a022df3088c647636a4745e98658fd2442f72c66
AKPatrashkov18 <AKPatrashkov18@codingburgas.bg>
2020-03-31 21:09:19 +0300

    Repair input errors in CreateTournamentMenu. Move date from struct HOUR into struct DATE.

--- struct PROJECT/struct PROJECT.cpp
+++ struct PROJECT/struct PROJECT.cpp
@@ -11,7 +11,7 @@ private:
 	short int tournamentCount = 0;
 	TOURNAMENT_INFO tournaments[100];
 
-public:	
+public:
 
 	void create(TOURNAMENT_INFO tournament) {
 		tournaments[tournamentCount++] = tournament;
@@ -40,56 +40,58 @@ public:
 
 	}
 
-	 void CreateTournamentMenu() {
+	void CreateTournamentMenu() {
 
 
-			TOURNAMENT_INFO ti;
-			cout << "\nEnter tournament name: ";
-			cin >> ti.name;
-			cout << "Enter date: ";
-			cout << "\nDay: ";
-			cin >> ti.date.day;
-			cout << "Month: ";
-			cin >> ti.date.month;
-			cout << "Year: ";
-			cin >> ti.date.year;
-			cout << "Enter start time:";
-			cout << "\nHour: ";
-			cin >> ti.startTime.hour;
-			cout << "Minutes: ";
-			cin >> ti.startTime.min;
-			cout << "Enter end time:";
-			cout << "\nHour: ";
-			cin >> ti.endTime.hour;
-			cout << "Minutes: ";
-			cin >> ti.endTime.min;
-			cout << "Enter tournament's prize: ";
-			cin >> ti.prize;
-			cout << "Team count: ";
-			cin >> ti.teamCount;
-			cout << "Enter how much players are in a team: ";
-			cin >> ti.playersOnTeam;
+		TOURNAMENT_INFO ti;
+		cout << "\nEnter tournament name: ";
+		getline(cin, ti.name);
+		cout << "Enter date: ";
+		cout << "\nDay: ";
+		cin >> ti.date.day;
+		cout << "Month: ";
+		cin >> ti.date.month;
+		cout << "Year: ";
+		cin >> ti.date.year;
+		cout << "Enter start time:";
+		cout << "\nHour: ";
+		cin >> ti.startTime.hour;
+		cout << "Minutes: ";
+		cin >> ti.startTime.min;
+		cout << "Enter end time:";
+		cout << "\nHour: ";
+		cin >> ti.endTime.hour;
+		cout << "Minutes: ";
+		cin >> ti.endTime.min;
+		cout << "Enter tournament's prize: ";
+		cin >> ti.prize;
+		cout << "Team count: ";
+		cin >> ti.teamCount;
+		cout << "Enter how much players are in a team: ";
+		cin >> ti.playersOnTeam;
 
-			for (int j = 0; j < ti.teamCount; j++)
+		for (int j = 0; j < ti.teamCount; j++)
+		{
+			cout << "Enter data for team " << j + 1 << ": \n";
+			cout << "Team name: ";
+			getline(cin, ti.teams[j].name);
+
+			cout << "Team tag: ";
+			cin >> ti.teams[j].tag;
+			getline(cin, ti.teams[j].tag);
+
+			cout << "Enter players' names\n";
+
+			for (int i = 1; i <= ti.playersOnTeam; i++)
 			{
-				cout << "Enter data for team " << j + 1<<": \n";
-				cout << "Team name: ";
-				cin >> ti.teams[j].name;
-				cout << "Team tag: ";
-				cin >> ti.teams[j].tag;
-				
-				cout << "Enter players' names\n";
-
-				for (int i = 1; i <= ti.playersOnTeam; i++)
-				{
-					cout << "Player " << i << ": ";
-					cin >> ti.teams[j].playerNames[i];
-				}
-
+				cout << "Player " << i << ": ";
+				getline(cin, ti.teams[j].playerNames[i]);
 			}
 
-			tournament.create(ti);
 		}
+
+		tournament.create(ti);
+	}
 
 	void MenuChoice() {
 		int choice;
@@ -112,7 +114,7 @@ int main()
 	//TOURNAMENT_INFO t = { "T1", { 1, 1, 1111 }, { 21, 0 }, { 23, 0 }, { { "team1", "t1", { "p1", "p2", "p3", "p4", "p5" } } }, "kupa" };
 
 	Tournament tournament;
-	
+
 	//tournament.create(t);
 
 	TournamentMenu menu(tournament);
